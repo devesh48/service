c0dcd59b51afd31a02558361d33ec96781c69f3d
devesh48 <upadhyay.dev48@gmail.com>
2018-03-29 12:04:53 +0530

    Change after Hello service completion.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const control = require ('../controller/myController');
 const ResponseHandler = require ('../controller/responseHandler');
 
-
 router.get('/service/hello',control.hello,ResponseHandler.sendResponse);
 router.get('/service/yourName:name', control.getYourName,ResponseHandler.sendResponse);//this will get executed first
 router.get('/service/getData:name',control.getMyData);
-router.get ('/service/yourName2:name',control.getYourName2);//same pattern is there. so whichever service is written first will execute first
+router.get ('/service/yourName2:name',control.getYourName2);//same url pattern as the 2nd one is there. so whichever service is written first will execute first
+
 
 module.exports = router;
